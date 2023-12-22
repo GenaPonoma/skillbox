5c8a5fcb2ea22df337ca5fd94ef4116db3860c79
gennadii_ponomarev <g.ponomareow2014@yandex.ru>
2023-12-22 21:19:10 +0300

    second part Project

--- m1_hello_world/app/src/main/java/com/example/homework/MainActivity.kt
+++ m1_hello_world/app/src/main/java/com/example/homework/MainActivity.kt
@@ -5,23 +5,32 @@ import android.os.Bundle
 import com.example.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val remainingSeats = 50;
-    var counter = 0;
+    private val remainingSeats = 50;
+    private var counter = 0;
+    private var countFreePlaces = remainingSeats
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if (counter == 0) binding.informationMessage.text = "Все места свободны"
         binding.placeCounter.text = counter.toString()
         binding.plusButton.setOnClickListener {
-            if (counter == 0) {
-                binding.placeCounter.text = counter++.toString()
-            }else if{
-
-            }
+            binding.placeCounter.text = counter++.toString()
+            binding.placeCounter.text = counter.toString()
+            countFreePlaces = сalculateAvailableSeats()
+            binding.informationMessage.text = "Колличество свободных мест: $countFreePlaces"
         }
         binding.minusButton.setOnClickListener {
             binding.placeCounter.text = counter--.toString()
+            binding.placeCounter.text = counter.toString()
+            countFreePlaces = сalculateAvailableSeats()
+            binding.informationMessage.text = "Колличество свободных мест: $countFreePlaces"
         }
+
+    }
+
+    private fun сalculateAvailableSeats(): Int {
+        return remainingSeats - counter
 
     }
 }
