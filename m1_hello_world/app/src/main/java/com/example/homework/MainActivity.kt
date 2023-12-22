db46622e2ad60db4ab1035f286fabf568260659e
gennadii_ponomarev <g.ponomareow2014@yandex.ru>
2023-12-22 14:12:58 +0300

    one part Project

--- m1_hello_world/app/src/main/java/com/example/homework/MainActivity.kt
+++ m1_hello_world/app/src/main/java/com/example/homework/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val remainingSeats = 50;
+    var counter = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.placeCounter.text = counter.toString()
+        binding.plusButton.setOnClickListener {
+            if (counter == 0) {
+                binding.placeCounter.text = counter++.toString()
+            }else if{
+
+            }
+        }
+        binding.minusButton.setOnClickListener {
+            binding.placeCounter.text = counter--.toString()
+        }
+
     }
 }
