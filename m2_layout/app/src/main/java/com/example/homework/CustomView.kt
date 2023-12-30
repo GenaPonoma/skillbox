c2da1b67ad8904d7dcb132f371da71882426677c
gennadii_ponomarev <g.ponomareow2014@yandex.ru>
2023-12-31 01:29:05 +0300

    completed my homework

--- m2_layout/app/src/main/java/com/example/homework/CustomView.kt
+++ m2_layout/app/src/main/java/com/example/homework/CustomView.kt
@@ -3,10 +3,27 @@ package com.example.homework
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import com.example.homework.databinding.CustomViewBinding
 
 class CustomView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
+
+
     LinearLayout(context, attrs) {
+    private  var  binding: CustomViewBinding
+
     init {
-        inflate(context, R.layout.activity_main, this)
+        val inflatedView = inflate(context, R.layout.custom_view, this)
+        binding = CustomViewBinding.bind(inflatedView)
     }
+
+
+    fun setMessageUpText(message: String) {
+        binding.upLineText.text = message
+    }
+
+    fun setMessageDownText(message: String) {
+        binding.downLineText.text = message
+    }
+
+
 }
--- m2_layout/app/src/main/java/com/example/homework/MainActivity.kt
+++ m2_layout/app/src/main/java/com/example/homework/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.customView.setMessageUpText("Верхняя строчка, настроенная из кода")
+        binding.customView.setMessageDownText("Нижняя строчка, настроенная из кода")
     }
 }
