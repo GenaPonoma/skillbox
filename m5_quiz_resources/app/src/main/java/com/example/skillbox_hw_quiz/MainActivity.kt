fe0f806e1b25114c4cbf831e50fe78bd1bd8e44d
gennadii_ponomarev <g.ponomareow2014@yandex.ru>
2024-02-12 13:44:08 +0300

    Completed homework

--- m5_quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.skillbox_hw_quiz
-
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.skillbox_hw_quiz.ui.main.MainFragment
+import com.example.skillbox_hw_quiz.databinding.MainActivityBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        var binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
     }
+
 }
