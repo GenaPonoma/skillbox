2b7f3f8e8e7ab2fda9e98dc839c79ce2c5489b6c
gennadii_ponomarev <g.ponomareow2014@yandex.ru>
2024-08-04 21:03:51 +0300

    Completed homework

--- m11_timer_data_storage/app/src/main/java/com/example/homework/MainActivity.kt
+++ m11_timer_data_storage/app/src/main/java/com/example/homework/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.homework
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homework.databinding.ActivityMainBinding
@@ -15,8 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         val repository = Repository(applicationContext)
-        binding.editText.setText(repository.getText())
+        binding.textResult.text = repository.getText()
+        binding.editText.addTextChangedListener(object: TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+                binding.textResult.text = s.toString()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    binding.textResult.text = s.toString()
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.textResult.text = s.toString()
+
+            }
+        })
+
+
         binding.saveButton.setOnClickListener {
                 repository.saveText(binding.editText.text.toString())
             Toast.makeText(this, "Текст сохранен", Toast.LENGTH_SHORT).show()
